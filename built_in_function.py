760e4afe9b1ea9fea9c0d8fd44670495918d0d35
mayankpriydarshi <mayankpriydarshi10@gmail.com>
2023-08-22 12:09:21 +0530

    py

---
+++ built_in_function.py
@@ -0,0 +1,13 @@
+# print function
+print("Mayank")
+print('golden')
+
+a = 10
+name = "Manu"
+print(a)
+print(name)   # print a declared variable
+
+print("Hello", "I am mayank", sep="---")    # sep=" " for separator
+
+print("Welcome", end=' ')  # ends the output with a space & start next print msg in same line
+print("Back")  # output= Welcome Back
