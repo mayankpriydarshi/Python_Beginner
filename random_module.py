a3f50a733006dbadb4202346a24698ed734923b3
mayankpriydarshi <mayankpriydarshi10@gmail.com>
2023-08-29 23:07:33 +0530

    py

---
+++ random_module.py
@@ -0,0 +1,8 @@
+import random
+
+# The randint() method returns an integer number selected element from the specified range.
+print(random.randint(3, 9))  # random.randint(start, stop)
+# The random() method returns a random floating number between 0 and 1.
+print(random.random())  # random.random()
+# The randrange() method returns a randomly selected element from the specified range.
+print(random.randrange(3, 9))  # random.randrange(start, stop, step)
