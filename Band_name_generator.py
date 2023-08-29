6b060866658c25670da03ecf64ad0d9a0a432b4f
mayankpriydarshi <mayankpriydarshi10@gmail.com>
2023-08-29 23:25:17 +0530

    py

---
+++ Band_name_generator.py
@@ -0,0 +1,9 @@
+# 1.Create a greeting for your program.
+print("Welcome to Band name generator")
+# 2.Ask the user for the city that they grew up in.
+city = input("Which city did you grew up in?\n")
+# 3.Ask the user for the name of a pet.
+pet = input("What is the name of your pet?\n")
+# 4.Combine the name of their city and pet and show them their band name.
+print("your band name chould be " + city + pet)
+# 5.Make sure the input cursor shows on a new line:
--- swap_number.py
+++ swap_number.py
@@ -1,5 +1,24 @@
+# without third veriable
 a = input("a: ")
 b = input("b: ")
 a, b = b, a
 print("a: " + a)
 print("b: " + b)
+# though XOR
+   # x = x ^ y
+   # y = x ^ y
+   # x = x ^ y
+# through temporary variable
+  # temp = x
+  # x = y
+  # y = temp
+  # print('The value of x after swapping: {}'.format(x))
+  # print('The value of y after swapping: {}'.format(y))
+# Addition and Subtraction
+  # x = x + y
+  # y = x - y
+  # x = x - y
+# Multiplication and Division
+  # x = x * y
+  # y = x / y
+  # x = x / y
