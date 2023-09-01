83cfe93a411eb0cd8ec5d68fd9de6ac86b3dd273
mayankpriydarshi <mayankpriydarshi10@gmail.com>
2023-09-02 00:31:24 +0530

    py

---
+++ factorial.py
@@ -0,0 +1,26 @@
+def factorial(n):
+    if n < 0:
+        return "Factorial is not defined for negative numbers"
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        result = 1
+        for i in range(2, n + 1):
+            result *= i
+        return result
+
+
+if __name__ == "__main__":
+    num = int(input("Enter a non-negative integer: "))
+    res = factorial(num)
+    print(res)
+
+# using recursion
+# def fact(n):
+#     if n == 1:
+#         return 1
+#     return n*fact(n-1)
+# if __name__ == "__main__":
+#     fact_input = int(input("Enter a number: "))
+#     result = fact(fact_input)
+#     print(result)
